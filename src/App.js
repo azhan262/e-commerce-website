aef20873954957918983d228f8b483ccbdaaced3
azhan262 <azhan261@gmail.com>
2023-04-17 13:50:01 +0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import {Routes , Route} from "react-router-dom"
+import Header from "./components/Header"
+import Cart from "./pages/Cart"
+import Photos from "./pages/Photos"
+import './App.css';
+
+function App() {    
+  return (
+      <div>
+          <Header />
+          <Routes>
+            <Route exact path="/" element={<Photos/>} />
+            <Route path="/cart" element = {<Cart />} />
+          </Routes>
+      </div>
+  )
+}
+
+export default App;
---
+++ src/hooks/useHover.js
@@ -0,0 +1,31 @@
+import {useState, useEffect, useRef} from "react"
+
+function useHover() {
+    const [hovered, setHovered] = useState(false)
+    const ref = useRef(null)
+    
+    function enter() {
+        setHovered(true)
+    }
+    
+    function leave() {
+        setHovered(false)
+    }
+    
+    useEffect(() => {
+        setTimeout(()=>{
+            ref.current.addEventListener("mouseenter", enter)
+            ref.current.addEventListener("mouseleave", leave)
+            
+            return () => {    
+                ref.current.removeEventListener("mouseenter", enter)
+                ref.current.removeEventListener("mouseleave", leave)
+            }
+        }, 500)
+        
+    }, [])
+    
+    return [hovered, ref]
+}
+
+export default useHover
